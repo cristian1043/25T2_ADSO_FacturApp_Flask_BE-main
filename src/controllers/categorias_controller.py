8fd34eec552a83eeb90e6949dc37ad3ba8ec92fc
DUBONACO <garavitojimenezcristian@gmail.com>
2026-07-16 13:56:18 -0500

    yes

--- src/controllers/categorias_controller.py
+++ src/controllers/categorias_controller.py
@@ -2,4 +2,39 @@ from src.models.categorias import Categorias
 
 
 class CategoriasController:
-    
+    @staticmethod
+    def get():
+        return Categorias.get()
+
+    @staticmethod
+    def get_by_id(id):
+        return Categorias.get_by_id(id)
+
+    @staticmethod
+    def save(data):
+        categoria = Categorias()
+        categoria.nombre = data["nombre"]
+        categoria.save()
+        return categoria
+
+    @staticmethod
+    def update(id, data):
+        categoria = Categorias.get_by_id(id)
+
+        if categoria is None:
+            return None
+
+        categoria.nombre = data["nombre"]
+        categoria.update()
+
+        return categoria
+
+    @staticmethod
+    def delete(id):
+        categoria = Categorias.get_by_id(id)
+
+        if categoria is None:
+            return False
+
+        categoria.delete()
+        return True
